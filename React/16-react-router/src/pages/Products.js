ffefce9dea94aba00baf328b929ba17660729f55
Jirat Chutrakul <87472782+Jiraties@users.noreply.github.com>
2021-12-15 23:25:38 +0700

    React JS | Router 1

--- React/16-react-router/src/pages/Products.js
+++ React/16-react-router/src/pages/Products.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = (props) => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <Link to="/products/p1">A Book</Link>
+        <Link to="/products/p2">A Carpet</Link>
+        <Link to="/products/p3">An Online Course</Link>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
---
+++ React/17-great-quotes/src/pages/QuoteDetails.js
@@ -0,0 +1,29 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning react is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning react is great!" },
+];
+
+const QuoteDetails = () => {
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetails;
